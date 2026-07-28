9ca08deaf85e23a82739fc19cef71f1ddc546ff4
KodehodeTor <torjarand.26pg@gmail.com>
2026-07-28 13:46:29 +0200

    Added click event from mouse with notes in script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Movable object:
 
 //Position for element:
-
 let x = 0;
 let y = 0;
 
@@ -21,13 +20,21 @@ document.addEventListener("keydown", (e) => {
     x -= 10;
   }
 
-  //We grab the element box, as a HTML element we access .style property and move it left/right/up/down from the loop above. We have the variables x and y above and move them 10 pixels accordingly to buttons being pressed.
+  //We grab the element box, as a HTML element we access .style property and move it left/right/up/down from the loop above. We have the variables x and y above and move box accordingly to buttons being pressed.
 
   box.style.left = x + "px";
   box.style.top = y + "px";
 });
 
-//Listens to click event from document, when the mouse is clicked down the box is moving the the selected position.
-// document.addEventListener("click", (e) {
-//     //Move element to clickable place
-// })
+// Listens to click event from document, when the mouse is clicked down the box is moving the the selected position.
+document.addEventListener("click", (e) => {
+  //Center the element by subtracting half its width and height
+  // e.pageX and e.pageY holds the exact horizontal and vertical cordinates of the mouse on the webpage.
+  // box.offsetWidth and height is the box size. We divide in half to find the center of the box.
+  const targetX = e.pageX - box.offsetWidth / 2;
+  const targetY = e.pageY - box.offsetHeight / 2;
+
+  // Move element to clickable place by using .style as before, we use template literals to get both the position and px, as CSS needs px to move the box.
+  box.style.left = `${targetX}px`;
+  box.style.top = `${targetY}px`;
+});
