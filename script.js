f4e4e71dbc83ee70db259aee9b817c85b2a1d2f3
KodehodeTor <torjarand.26pg@gmail.com>
2026-07-28 13:09:00 +0200

    Removed pseduo code, added y,x  in if else if loop

--- script.js
+++ script.js
@@ -8,21 +8,26 @@ let y = 0
 // Box element:
 const box = document.querySelector("#box")
 
-//PSEDUO CODE:
 
+//Listens to key event from document, when key is pressed down enable function, if keys are pressed move x y from orginal place. Start position is 0,0 X = how far from left side, Y = how far from the top. The box moves by 10 pixels for each key stroke.
 
-//Listens to event from document, when key is pressed down enable function, if keys are pressed move x y from orginal place.
 document.addEventListener("keydown", (e) => {
-    if (e.key === "Arrowup")
-    // PSEDUO { move element up }
-    else if (e.key === "Arrowdown")
-    // PSEDUO { move element down }
+    if (e.key === "ArrowUp") 
+        {y -= 10;}
+    else if (e.key === "ArrowDown")
+        {y += 10}
      else if (e.key === "ArrowRight")
-    // PSEDUO { move element right }
+        {x += 10}
      else if (e.key === "ArrowLeft")
-    // PSEDUO { move element left }
+        {x -= 10;}
+
+     //The box only moves when we tell CSS about it.
+    box.style.left = x + "px";
+    box.style.top = y + "px";
 })
 
+
+//Listens to click event from document, when the mouse is clicked down the box is moving the the selected position.
 document.addEventListener("click", (e) {
     //Move element to clickable place
 })
