6fb1f09f177d8391bd11bd028b095c35a536f6cd
KodehodeTor <torjarand.26pg@gmail.com>
2026-07-28 12:45:37 +0200

    Pseduo code  logic implemented

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Movable object:
+
+//PSEDUO CODE:
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "up")
+    // PSEDUO { move element up }
+    else if (e.key === "down")
+    // PSEDUO { move element down }
+     else if (e.key === "right")
+    // PSEDUO { move element right }
+     else if (e.key === "left")
+    // PSEDUO { move element left }
+})
+
